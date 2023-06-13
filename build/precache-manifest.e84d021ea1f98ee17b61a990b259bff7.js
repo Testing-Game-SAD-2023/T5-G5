18c88f9cdb2b3888db7f2205397526c35b799208
angiolye24 <72791760+angiolye24@users.noreply.github.com>
2023-06-13 12:03:05 +0200

    Add files via upload

---
+++ build/precache-manifest.e84d021ea1f98ee17b61a990b259bff7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "14d7c48256165c26346e",
+    "url": "/static/css/main.1ecc6fcd.chunk.css"
+  },
+  {
+    "revision": "14d7c48256165c26346e",
+    "url": "/static/js/main.14d7c482.chunk.js"
+  },
+  {
+    "revision": "8f9271c1a84bbcede60e",
+    "url": "/static/js/1.8f9271c1.chunk.js"
+  },
+  {
+    "revision": "8b74bb926ba5a6c17e41",
+    "url": "/static/css/2.30ee2eba.chunk.css"
+  },
+  {
+    "revision": "8b74bb926ba5a6c17e41",
+    "url": "/static/js/2.8b74bb92.chunk.js"
+  },
+  {
+    "revision": "c8a0884bf480a201e796",
+    "url": "/static/js/runtime~main.c8a0884b.js"
+  },
+  {
+    "revision": "21c5ea6585428881a2f92f3e24ef4dbd",
+    "url": "/index.html"
+  }
+];
